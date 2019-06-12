24849bde9eec1e12a8c95596f218581d853cf4f9
maoyu <maoyu417@gmail.com>
2019-06-12 11:36:10 +0800

    动态设置首页日期导航

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,11 +1,18 @@
 // pages/home/home.js
-
+let util = require('../../utils/util.js')
+let shopModel = require('../../model/shop.js')
+let map = new Map()
+let data = []
+let currentDate = null
+let shop = null
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    date:'',  // 时间导航日期
+    week:'',  // 时间导航星期
     showHelpOrderView:false,
     showOrderView:false,
     showPaymentView:false
@@ -18,6 +25,11 @@ Page({
     wx.setNavigationBarTitle({
       title: getApp().buddysoft.name
     })
+
+    shop = shopModel.getShopInfo()
+    currentDate = util.today()
+    this.initDate(currentDate)
+    this.initWorktimeList()
   },
 
   /**
@@ -126,5 +138,32 @@ Page({
       showOrderView: false,
       showPaymentView: false
     })
+  },
+
+  initDate:function(date) {
+    this.setData({
+      date:util.formatDate(date),
+      week:util.week(date)
+    })
+  },
+
+  initWorktimeList:function() {
+    this.initWorktime()
+  },
+
+  initWorktime:function() {
+    console.log(shop)
+    console.log(currentDate)
+    if (shop) {
+      let workTimeBegin = shop
+        data.push({'a':10,'b':20})
+        map.set(1,data[0])
+      data[0].a = 30
+        console.log(map)
+    }
+  },
+
+  makeDatetime:function(time) {
+
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,21 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+/**
+ * 今天
+ */
+function today() {
+  let date = new Date()
+  let year = date.getFullYear()
+  let month = date.getMonth() + 1
+  let day = date.getDate()
+
+  return [year, month, day].map(formatNumber).join('-')
+}
+
+/**
+ * 后天
+ */
 function afterTomorrow() {
   let date = new Date()
   let year = date.getFullYear()
@@ -38,9 +53,23 @@ function formatTime(time) {
   return time[0] + ":" + time[1]
 }
 
+function week(date) {
+  date = date.split('-')
+  date = new Date(date[0], date[1], date[2])
+
+  console.log(date)
+
+  let weeks = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
+  let day = date.getDay()
+  
+  return weeks[day]
+}
+
 module.exports = {
   // formatTime: formatTime,
+  today: today,
   afterTomorrow: afterTomorrow,
+  week: week,
   formatDate: formatDate,
   formatTime: formatTime
 }
